792c970b17a02f669494794bbbf917f9286adfca
Eugene Chernousov <evgeny.chernousov@ya.ru>
2022-06-05 01:46:05 +0300

    Modal pop-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +12,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { AlarmComponent } from './alarm/alarm.component';
 import { MedicPageComponent } from './medic-page/medic-page.component';
 import {ContentService} from './content/content.service';
+import {FormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,9 @@ import {ContentService} from './content/content.service';
     BrowserAnimationsModule,
     FlexModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [ContentService],
   bootstrap: [AppComponent]
--- src/app/medic-page/medic-page.component.ts
+++ src/app/medic-page/medic-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-medic-page',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicPageComponent implements OnInit {
 
-  constructor() { }
+  currentOperation: any;
 
-  ngOnInit() {
+  constructor(private modalService: NgbModal) {
+
   }
 
+  ngOnInit() {
+
+  }
+
+  openMultiModal(multiModal: any, type: any) {
+    if (type === 1) {
+      this.currentOperation = 'Отчет о медосмотре';
+    }
+    else {
+        this.currentOperation = 'Направление дино на лечение';
+    }
+    console.log(multiModal)
+    this.modalService.open(multiModal).result
+      .then((result) => console.log('Modal closed'))
+      .catch(err => '');
+  }
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);
+    // const url = 'http://localhost:8888/friends/addnew';
+    // this.httpClient.post(url, f.value)
+    //   .subscribe((result) => {
+    //     this.ngOnInit(); //reload the table
+    //   });
+    this.modalService.dismissAll(); //dismiss the modal
+  }
 }
